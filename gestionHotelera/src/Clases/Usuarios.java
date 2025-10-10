d94d357d03ae8cf03247a8439323f1d1c0902e2e
SolPomares <lamalaya1980@gmail.com>
2025-10-10 13:06:39 -0300

    Clase abstracta desarrollada

--- gestionHotelera/src/Clases/Usuarios.java
+++ gestionHotelera/src/Clases/Usuarios.java
@@ -1,5 +1,58 @@
 package Clases;
 
 public abstract class Usuarios {
-    
+    // datos generales
+    private static int contador = 0;
+    private int idUsuario;
+    private String nombre;
+    private String apellido;
+    private int dni;
+
+    //constructor
+    public Usuarios(int idUsuario, String nombre, String apellido, int dni) {
+        this.idUsuario = contador ++;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+    }
+
+    //constructor minimo
+
+
+    public Usuarios(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    //Getters y setters
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    //metodos
+    public abstract String imprimirDatos();
 }
