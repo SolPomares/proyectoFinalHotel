8d195b114695cc6f694f5cb067c5c5f96ce7025e
SolPomares <lamalaya1980@gmail.com>
2025-10-10 14:36:48 -0300

    metodo permiso en administrador

--- gestionHotelera/src/Clases/Administrador.java
+++ gestionHotelera/src/Clases/Administrador.java
@@ -40,11 +40,12 @@ private String claveAdministracion;
       System.out.println(" BAK UP REALIZADO CON EXITO");
   }
 
-  public void asignarPermisos(SistemaHotel sistemaHotel, Empleados e, boolean tieneAcceso) {
+  public boolean asignarPermisos(SistemaHotel sistemaHotel, Empleados e, boolean tieneAcceso) {
       //Esto se me complico porque el admin otorga el acceso y lo delega a al sistemade gestion)
-      String estado = tieneAcceso ? "OTORGADO : REVOCADO";
+      boolean gestionExitosa = sistemaHotel.gestionarPermisoAcceso(empleado, tieneAcceso);
+      if (tieneAcceso) estado = "OTORGADO : REVOCADO";
       System.out.println("estado = " + estado);
-
+return estado;
   }
 
 
