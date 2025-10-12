8961d2aa276c706310f3cefb654353c9942bb45c
SolPomares <lamalaya1980@gmail.com>
2025-10-12 10:06:41 -0300

    Crear Archivo + iosexception en SistemaHotel

--- gestionHotelera/src/Clases/SistemaHotel.java
+++ gestionHotelera/src/Clases/SistemaHotel.java
@@ -1,7 +1,10 @@
 package Clases;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.io.*;
+import java.util.InputMismatchException;
 
 public class SistemaHotel {
     private List<Empleados> listaEmpleados;
@@ -11,6 +14,7 @@ public class SistemaHotel {
         this.listaEmpleados = new ArrayList<>();
     }
 
+    //getters y setters
     public List<Empleados> getListaEmpleados() {
         return listaEmpleados;
     }
@@ -20,6 +24,7 @@ public class SistemaHotel {
     }
 
     // Metodos
+
 
     public boolean gestionarPermisoAcceso(Empleados empleado) {
         if (!empleado.tienePermisoSistema()) {
@@ -50,5 +55,19 @@ public class SistemaHotel {
         System.out.println(" El ACCESO A CAMBIADO");
         /// OJO A GUARDAR LO DE JSON PERSISTENCIA DE DATOS - BASE DE DATOS EMPLEADOS
     }*/
+
+    public static void crearArchivo(String nombreArchivo)throws IOException {
+        try{
+            File archivo = new File(nombreArchivo); //tb puede ir la ruta del archivo
+            PrintWriter escritura = new PrintWriter(archivo);//escribe el archivo
+            escritura.close();
+            System.out.println("El archivo: "+ nombreArchivo+" se creo correctamente");
+        } catch (FileNotFoundException ex) {
+            System.out.println("No se puede Escribir el archivo");
+            throw new IOException(ex);
+        }
+    }
+
+
 }
 
