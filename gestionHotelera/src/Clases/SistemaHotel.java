a7bc3ffae806dccb050fecc8382e7114ed8c4d0e
SolPomares <lamalaya1980@gmail.com>
2025-10-13 13:32:55 -0300

    Se vuelven a cargar los archivos que vienen anidados con .idea

---
+++ gestionHotelera/src/Clases/SistemaHotel.java
@@ -0,0 +1,73 @@
+package Clases;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.io.*;
+import java.util.InputMismatchException;
+
+public class SistemaHotel {
+    private List<Empleados> listaEmpleados;
+
+    //CONSTRUCTOR
+    public SistemaHotel(List<Empleados> listaEmpleados) {
+        this.listaEmpleados = new ArrayList<>();
+    }
+
+    //getters y setters
+    public List<Empleados> getListaEmpleados() {
+        return listaEmpleados;
+    }
+
+    public void setListaEmpleados(List<Empleados> listaEmpleados) {
+        this.listaEmpleados = listaEmpleados;
+    }
+
+    // Metodos
+
+
+    public boolean gestionarPermisoAcceso(Empleados empleado) {
+        if (!empleado.tienePermisoSistema()) {
+            System.out.println("ACCESO DENEGADO: La cuenta de " + empleado.getNombreUsuario() +
+                    " no está activa o no tiene permisos de acceso al sistema.");
+            return false;
+        } else {  // Si tiene permiso, valido la contraseña
+            System.out.println("--------------------- AUTENTICACIÓN ----------------------");
+            Scanner teclado = new Scanner(System.in);
+
+            System.out.print("Usuario: " + empleado.getNombreUsuario() + "\n");
+            System.out.print("Ingrese su contraseña: ");
+            String contraseniaIngresada = teclado.nextLine();
+
+            if (empleado.getContrasenia().equals(contraseniaIngresada)) {
+                System.out.println("\nACCESO EXITOSO. Bienvenido(a), " + empleado.getNombreUsuario() + ".");
+                return true;
+            } else {
+                System.out.println("\nACCESO DENEGADO: Contraseña incorrecta.");
+                return false;
+            }
+        }
+    }
+
+   /* public void gestionarPermisoAcceso(Empleados empleado, boolean tieneAcceso) {
+        // 1. Ejecuta setter de acceso de Empleados
+        empleado.setAcceso(tieneAcceso);
+        System.out.println(" El ACCESO A CAMBIADO");
+        /// OJO A GUARDAR LO DE JSON PERSISTENCIA DE DATOS - BASE DE DATOS EMPLEADOS
+    }*/
+
+    public static void crearArchivo(String nombreArchivo)throws IOException {
+        try{
+            File archivo = new File(nombreArchivo); //tb puede ir la ruta del archivo
+            PrintWriter escritura = new PrintWriter(archivo);//escribe el archivo
+            escritura.close();
+            System.out.println("El archivo: "+ nombreArchivo+" se creo correctamente");
+        } catch (FileNotFoundException ex) {
+            System.out.println("No se puede Escribir el archivo");
+            throw new IOException(ex);
+        }
+    }
+
+
+}
+
---
+++ gestionHotelera/src/Clases/Usuarios.java
@@ -0,0 +1,73 @@
+package Clases;
+
+import Enums.TipoRol;
+
+public abstract class Usuarios {
+    // datos generales
+    private static int contador = 0;
+    private int idUsuario;
+    private String nombre;
+    private String apellido;
+    private int dni;
+    private TipoRol tipoRol;
+
+
+    //constructor
+    public Usuarios(int idUsuario, String nombre, String apellido, int dni, TipoRol tipoRol) {
+        this.idUsuario = contador ++;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.tipoRol = tipoRol;
+
+    }
+
+    //constructor minimo
+
+
+    public Usuarios(String nombre, String apellido,TipoRol tipoRol) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.tipoRol = tipoRol;
+    }
+
+    //Getters y setters
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public TipoRol getTipoRol() {
+        return tipoRol;
+    }
+
+    public void setTipoRol(TipoRol tipoRol) {
+        this.tipoRol = tipoRol;
+    }
+
+    //metodos
+    public abstract void imprimirDatos();
+}
