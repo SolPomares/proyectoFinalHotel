aca89d60689cf040d6e41e253536d7c18110222e
MerlinaFernandez <fernandezmerlina17@gmail.com>
2025-10-13 12:35:21 -0300

    Constructor, getters y setters de Habitacion

--- gestionHotelera/src/Clases/Habitacion.java
+++ gestionHotelera/src/Clases/Habitacion.java
@@ -1,10 +1,51 @@
 public class Habitacion {
     //Atributos
+    private int numero;
     private double precioNoche;
     private TipoHabitacion tipo;
     private boolean disponible;
 
+    //Constructor
+    public Habitacion(int numero, double precioNoche, TipoHabitacion tipo, boolean disponible) {
+        this.numero = numero;
+        this.precioNoche = precioNoche;
+        this.tipo = tipo;
+        this.disponible = disponible;
+    }
+
+    //Gets y sets
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getPrecioNoche() {
+        return precioNoche;
+    }
+
+    public void setPrecioNoche(double precioNoche) {
+        this.precioNoche = precioNoche;
+    }
+
+    public TipoHabitacion getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoHabitacion tipo) {
+        this.tipo = tipo;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
 
 
-
+    
 }
