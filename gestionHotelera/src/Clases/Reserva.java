c30f6eddca14cc068e21220f42bc7b96883ed1c2
MerlinaFernandez <fernandezmerlina17@gmail.com>
2025-11-05 20:48:05 -0300

    Constructor y gets, sets de la clase Reserva

--- gestionHotelera/src/Clases/Reserva.java
+++ gestionHotelera/src/Clases/Reserva.java
@@ -9,7 +9,49 @@ public class Reserva {
     private Date checkIn;
     private Date checkOut;
 
+    //Constructor
+    public Reserva( Pasajero pasajero, Habitacion habitacion, Date checkIn, Date checkOut) {
+        this.idReserva = UUID.randomUUID();
+        this.pasajero = pasajero;
+        this.habitacion = habitacion;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
 
+    //Gets y sets
+    public UUID getIdReserva() {
+        return idReserva;
+    }
+    
+    public Pasajero getPasajero() {
+        return pasajero;
+    }
 
+    public void setPasajero(Pasajero pasajero) {
+        this.pasajero = pasajero;
+    }
 
+    public Habitacion getHabitacion() {
+        return habitacion;
+    }
+
+    public void setHabitacion(Habitacion habitacion) {
+        this.habitacion = habitacion;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(Date checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(Date checkOut) {
+        this.checkOut = checkOut;
+    }
 }
