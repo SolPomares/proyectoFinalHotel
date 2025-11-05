c5b60266485cf7edd2d2943d57419a7a69ef9d19
MerlinaFernandez <fernandezmerlina17@gmail.com>
2025-11-05 20:45:36 -0300

    Atributos de la clase Reserva

--- gestionHotelera/src/Clases/Reserva.java
+++ gestionHotelera/src/Clases/Reserva.java
@@ -1,2 +1,15 @@
+import java.util.Date;
+import java.util.UUID;
+
 public class Reserva {
+    //Atributos
+    private UUID idReserva;
+    private Pasajero pasajero;
+    private Habitacion habitacion;
+    private Date checkIn;
+    private Date checkOut;
+
+
+
+
 }
