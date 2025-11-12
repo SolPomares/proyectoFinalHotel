730f5f17a0b823929d4e3e5fcd795f7c47c723bc
resetjoel <joeljr.beteta@hotmail.com>
2025-11-12 01:41:05 -0300

    Fondo de pantalla temporal y base estable antes del carrusel

--- src/main/java/com/hotel/front/FrontApp.java
+++ src/main/java/com/hotel/front/FrontApp.java
@@ -1,4 +1,26 @@
 package com.hotel.front;
 
-public class FrontApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FrontApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(
+                getClass().getResource("/com/hotel/front/PublicView.fxml")
+        );
+
+        Scene scene = new Scene(root, 900, 600);
+        stage.setTitle("Hotel - pantalla pública");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
