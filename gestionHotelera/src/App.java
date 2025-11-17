274a96f35b95ab5bbe4a2f40689b8eb5f0cdc851
SolPomares <lamalaya1980@gmail.com>
2025-11-17 13:51:19 -0300

    buscando error en carga de admin

--- gestionHotelera/src/App.java
+++ gestionHotelera/src/App.java
@@ -69,7 +69,7 @@ public class App {
                 //Aqui simplificamoes metodo para que si o si cargu un usuario inicial y no quede null que se arrastra
                 gestorUsuarios.agregar(adminInicial);//falta hacer el metodo para empleado asi q use este porque el sistemaHotel acepta t elemntos
                 System.out.println("[INFO] Administrador genérico ('admin'/'admin123') cargado.");
-            } catch (datoInvalidoException e) {
+            } catch (Exception e) { // <-- Cambiado de datoInvalidoException a Exception
                 System.err.println("[ERROR] Error al cargar Admin inicial: " + e.getMessage());
             }
         }
